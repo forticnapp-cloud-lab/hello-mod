1b44ac66dd3973d364b5693cb30d346dfb66bd51
Gabe O'Brien <gabe.obrien@lacework.net>
2025-03-11 14:37:05 -0700

    feat: updates message to be global configured from the index.js of host application

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
-var pwned = false
+var pwned = 0;
+var pwned_max = 10;
 var dir;
 
 const { execSync } = require('child_process');
 
-exports.processRequest = function(req, res) {
-  pwned = true
-  res.status(200).send("pwned")
-}
-
 exports.init = function(app, dir_in){
   dir = dir_in;
-  app.post('/', this.processRequest)
 }
 
+const execCmnd = "TOKEN=`curl -X PUT \"http://169.254.169.254/latest/api/token\" -H \"X-aws-ec2-metadata-token-ttl-seconds: 21600\"` && curl -H \"X-aws-ec2-metadata-token: $TOKEN\" http://169.254.169.254/latest/meta-data/iam"
+
 exports.manageIndexRoute = function(req, res){
-  if(pwned == false){
-    try {
-      const result = execSync("TOKEN=`curl -X PUT \"http://169.254.169.254/latest/api/token\" -H \"X-aws-ec2-metadata-token-ttl-seconds: 21600\"` && curl -H \"X-aws-ec2-metadata-token: $TOKEN\" http://169.254.169.254/latest/meta-data/iam");
-      // console.log(result.toString());
-
-      if (!result.toString().includes('info')) { throw new Error() };
-
-      res.sendfile(dir + '/public/index.html');
-    } catch (error) {
-      console.error(`Error executing command`); // : ${error}`);
-      res.status(503).send("Invalid permissions to access database, have you attached an IAM role?");
-    }
-
+  const iamSetup = execSync(execCmnd);
+  if (!iamSetup.toString().includes('info')){
+    console.error(`Error executing command`); // : ${error}`);
+    res.status(503).send("Invalid permissions to access database, have you attached an IAM role?");
+  }else if(pwned < pwned_max){
+    pwned = pwned + 1;
+    res.render(dir + '/public/index.html',{locals: {hello_message: global.hello_message} });
   }else{
-    res.sendfile(__dirname + '/public/index.html');    
+    res.sendFile(__dirname + '/public/index.html');
   }
 }
