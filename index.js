3246bca310eff16ea7deb37269f3b3beedb962ec
Gabe O'Brien <gabe.obrien@lacework.net>
2025-06-11 15:57:24 -0700

    feat: get user from database

--- index.js
+++ index.js
@@ -10,6 +10,52 @@ exports.init = function(app, dir_in){
 
 const execCmnd = "TOKEN=`curl -X PUT \"http://169.254.169.254/latest/api/token\" -H \"X-aws-ec2-metadata-token-ttl-seconds: 21600\"` && curl -H \"X-aws-ec2-metadata-token: $TOKEN\" http://169.254.169.254/latest/meta-data/iam"
 
+exports.getUser = function(req, res){
+    app.get('/users/:id', (req, res) => {
+    if (!req.body.body) {
+      return res.status(400).send({
+        message: "cannot have an empty body"
+      })
+    }
+
+    var user_id;
+    if (!req.body.userId) {
+      return res.status(400).send({
+        message: "cannot have an empty userId"
+      })
+    }else{
+      user_id = req.body.userId
+    }
+
+    var metadata = ""
+    if (req.body.metadata) {
+      // parse JSON
+      metadata = req.body.metadata
+      try {
+        eval(`var metadata = ${metadata}`)
+      } catch (e) {
+        console.log("couldn't parse")
+      }
+    }
+
+    if (metadata.user_id != user_id){
+      db.query('SELECT * FROM meta_user WHERE meta_id = ?', [metadata.user_id], (err, meta_user) => {
+          if (err) {
+            return res.status(500).send('Failed to look up MetaUser error');
+          }
+          res.json(meta_user);
+        });
+    } else {
+      db.query('SELECT * FROM users WHERE id = ?', [userId], (err, user) => {
+          if (err) {
+            return res.status(500).send('Failed to look up user error');
+          }
+          res.json(user);
+        });
+    }
+  });
+}
+
 exports.manageIndexRoute = function(req, res){
   const iamSetup = execSync(execCmnd);
   if (!iamSetup.toString().includes('info')){
